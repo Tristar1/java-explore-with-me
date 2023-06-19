949ebaa89a74ea21a37ca534ac9e4b99d25082bc
Serg <sergstar777@mail.ru>
2023-06-19 15:41:40 +0300

    ready stage 3 fixed 12

--- main/src/main/java/ru/practicum/dto/Comment/CommentDto.java
+++ main/src/main/java/ru/practicum/dto/Comment/CommentDto.java
@@ -1,9 +1,11 @@
 package ru.practicum.dto.Comment;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.practicum.constants.DateTimeFormat;
 import ru.practicum.dto.Event.EventShortDto;
 import ru.practicum.dto.User.UserShortDto;
 
@@ -19,9 +21,11 @@ public class CommentDto {
     private String text;
     private EventShortDto event;
     private UserShortDto author;
+    @JsonFormat(pattern = DateTimeFormat.DATE_TIME_FORMAT)
     private Timestamp modifyDate;
     private Boolean modified;
     private Boolean visible;
+    @JsonFormat(pattern = DateTimeFormat.DATE_TIME_FORMAT)
     private Timestamp publicationDate;
 
 }
